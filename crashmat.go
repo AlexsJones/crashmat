f97388d76d85b224f75ae81a91b8a7a6cf8e8a34
Alexsjones <tibbar@freedommail.ch>
2015-09-25 11:23:13 +0100

    adding workspace

--- crashmat.go
+++ crashmat.go
@@ -2,53 +2,53 @@
 *     File Name           :     crashmat.go
 *     Created By          :     anon
 *     Creation Date       :     [2015-09-24 23:14]
-*     Last Modified       :     [2015-09-25 11:13]
+*     Last Modified       :     [2015-09-25 11:23]
 *     Description         :
 **********************************************************************************/
 package main
 
 import (
-	"github.com/stretchr/goweb"
-	"log"
-	"net"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
+  "github.com/stretchr/goweb"
+  "log"
+  "net"
+  "net/http"
+  "os"
+  "os/signal"
+  "time"
 )
 
 const (
-	Address string = ":9090"
+  Address string = ":9090"
 )
 
 func main() {
 
-	mapRoutes()
+  mapRoutes()
 
-	log.Print("Initialising...")
-	s := &http.Server{
-		Addr:           Address,
-		Handler:        goweb.DefaultHttpHandler(),
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
+  log.Print("Initialising...")
+  s := &http.Server{
+    Addr:           Address,
+    Handler:        goweb.DefaultHttpHandler(),
+    ReadTimeout:    10 * time.Second,
+    WriteTimeout:   10 * time.Second,
+    MaxHeaderBytes: 1 << 20,
+  }
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	listener, listenErr := net.Listen("tcp", Address)
+  c := make(chan os.Signal, 1)
+  signal.Notify(c, os.Interrupt)
+  listener, listenErr := net.Listen("tcp", Address)
 
-	log.Printf("  visit: %s", Address)
-	if listenErr != nil {
-		log.Fatalf("Could not listen: %s", listenErr)
-	}
+  log.Printf("  visit: %s", Address)
+  if listenErr != nil {
+    log.Fatalf("Could not listen: %s", listenErr)
+  }
 
-	go func() {
-		for _ = range c {
+  go func() {
+    for _ = range c {
 
-			listener.Close()
-		}
+      listener.Close()
+    }
 
-	}()
-	log.Fatalf("Error in server: %s", s.Serve(listener))
+  }()
+  log.Fatalf("Error in server: %s", s.Serve(listener))
 }
--- routes.go
+++ routes.go
@@ -2,26 +2,26 @@
 *     File Name           :     routes.go
 *     Created By          :     anon
 *     Creation Date       :     [2015-09-25 09:51]
-*     Last Modified       :     [2015-09-25 11:13]
+*     Last Modified       :     [2015-09-25 11:23]
 *     Description         :
 **********************************************************************************/
 package main
 
 import (
-	"github.com/stretchr/goweb"
-	"github.com/stretchr/goweb/context"
-	"log"
+  "github.com/stretchr/goweb"
+  "github.com/stretchr/goweb/context"
+  "log"
 )
 
 func mapRoutes() {
 
-	goweb.MapBefore(func(c context.Context) error {
-		log.Printf("%s %s %s", c.HttpRequest().RemoteAddr, c.MethodString(), c.HttpRequest().URL.Path)
-		return nil
-	})
+  goweb.MapBefore(func(c context.Context) error {
+    log.Printf("%s %s %s", c.HttpRequest().RemoteAddr, c.MethodString(), c.HttpRequest().URL.Path)
+    return nil
+  })
 
-	goweb.Map("GET", "/", func(c context.Context) error {
-		return goweb.Respond.WithOK(c)
-	})
+  goweb.Map("GET", "/", func(c context.Context) error {
+    return goweb.Respond.WithOK(c)
+  })
 
 }
