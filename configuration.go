ea42a828a959cb8dce18e4286e008d55df3bc1f8
Alexsjones <tibbar@freedommail.ch>
2015-09-25 11:49:39 +0100

    added config file

---
+++ configuration.go
@@ -0,0 +1,33 @@
+/*********************************************************************************
+*     File Name           :     configuration.go
+*     Created By          :     anon
+*     Creation Date       :     [2015-09-25 11:33]
+*     Last Modified       :     [2015-09-25 11:47]
+*     Description         :      
+**********************************************************************************/
+
+package main
+
+import (
+  "os"
+  "encoding/json"
+  "log"
+)
+
+type Configuration struct {
+  Port string
+}
+
+func NewConfiguration(configurationPath string) *Configuration {
+  conf,err := os.Open(configurationPath)
+  if err != nil {
+    log.Fatalf("opening configuration file",err.Error())
+  }
+
+  var configuration Configuration
+  jsonParser := json.NewDecoder(conf)
+  if err = jsonParser.Decode(&configuration); err != nil {
+    log.Fatalf("parsing config file", err.Error())
+  }
+  return &configuration
+}
